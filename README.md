6ce95bba4083bfb007ef6024fab32ef50697b655
Benjamin Floyd Rubin <bennyfrubin@gmail.com>
2020-06-25 21:58:19 -0700

    Add files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Google Calendar Event Creator (Chrome Extension)
 A chrome extension for creating Google Calendar events using free form text!
+
---
+++ background.js
@@ -0,0 +1,9 @@
+// Copyright 2020 Benjamin Floyd Rubin. All rights reserved.
+
+'use strict';
+
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.storage.sync.set({color: '#3aa757'}, function() {
+    console.log("The color is green.");
+  });
+});
